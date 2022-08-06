4a6e420576cd0c2cf363a984c8189a740fb89340
Wolf Hu <arieshu700329@gmail.com>
2022-08-07 06:42:28 +0800

    1.0.6
    1. Adding images for webpage.
    2.Adding footer for webpage.

--- script/vm.js
+++ script/vm.js
@@ -7,21 +7,21 @@ const app = Vue.createApp({
                         'carousel-item': true,
                         active: true,
                     },
-                    src: 'https://fakeimg.pl/2048x892/?text=Img1(2048x892)',
+                    src: 'images\\carousel_1.jpg',
                 },
                 {
                     classesLst: {
                         'carousel-item': true,
                         active: false,
                     },
-                    src: 'https://fakeimg.pl/2048x892/?text=Img2(2048x892)',
+                    src: 'images\\carousel_2.jpg',
                 },
                 {
                     classesLst: {
                         'carousel-item': true,
                         active: false,
                     },
-                    src: 'https://fakeimg.pl/2048x892/?text=Img3(2048x892)',
+                    src: 'images\\carousel_3.jpg',
                 },
             ],
         };
@@ -34,19 +34,19 @@ const app2 = Vue.createApp({
             coffeeBeanTypes: [
                 {
                     classLst: ['col'],
-                    imgSrc: 'https://fakeimg.pl/600x600/?text=600x600(1)',
+                    imgSrc: 'images\\coffee_bean_1.jpg',
                     typeName: '單品配方',
                     price: 'NT$285 - NT$650',
                 },
                 {
                     classLst: ['col'],
-                    imgSrc: 'https://fakeimg.pl/600x600/?text=600x600(2)',
+                    imgSrc: 'images\\coffee_bean_2.jpg',
                     typeName: '複合配方',
                     price: 'NT$310 - NT$710',
                 },
                 {
                     classLst: ['col', 'd-none', 'd-md-block'],
-                    imgSrc: 'https://fakeimg.pl/600x600/?text=600x600(3)',
+                    imgSrc: 'images\\coffee_bean_3.jpg',
                     typeName: '春夏配方',
                     price: 'NT$500 - NT$1050',
                 },
@@ -54,28 +54,28 @@ const app2 = Vue.createApp({
             latestProducts: [
                 {
                     classLst: ['col'],
-                    imgSrc: 'https://fakeimg.pl/600x600/?text=600x600(1)',
+                    imgSrc: 'images\\T-shirt.jpg',
                     name: 'T-shirt',
                     pricing: 'NT$3680',
                     actualPrice: 'NT$2944',
                 },
                 {
                     classLst: ['col'],
-                    imgSrc: 'https://fakeimg.pl/600x600/?text=600x600(2)',
+                    imgSrc: 'images\\杯子.jpg',
                     name: '馬克杯',
                     pricing: 'NT$1400',
                     actualPrice: 'NT$1120',
                 },
                 {
                     classLst: ['col', 'd-none', 'd-md-block'],
-                    imgSrc: 'https://fakeimg.pl/600x600/?text=600x600(3)',
+                    imgSrc: 'images\\帽子.jpg',
                     name: '棒球帽',
                     pricing: 'NT$2300',
                     actualPrice: 'NT$1904',
                 },
                 {
                     classLst: ['col', 'd-none', 'd-md-block'],
-                    imgSrc: 'https://fakeimg.pl/600x600/?text=600x600(4)',
+                    imgSrc: 'images\\購物袋.jpg',
                     name: '購物袋',
                     pricing: 'NT$1800',
                     actualPrice: 'NT$1440',
@@ -84,43 +84,43 @@ const app2 = Vue.createApp({
             productTypes: [
                 {
                     classLst: ['col', 'mt-2'],
-                    imgSrc: 'https://fakeimg.pl/600x600/?text=600x600(1)',
+                    imgSrc: 'images\\咖啡豆.jpg',
                     name: '咖啡豆',
                 },
                 {
                     classLst: ['col', 'mt-2'],
-                    imgSrc: 'https://fakeimg.pl/600x600/?text=600x600(2)',
+                    imgSrc: 'images\\濾泡咖啡.jpg',
                     name: '濾泡咖啡',
                 },
                 {
                     classLst: ['col', 'mt-2'],
-                    imgSrc: 'https://fakeimg.pl/600x600/?text=600x600(3)',
+                    imgSrc: 'images\\咖啡器具.jpg',
                     name: '咖啡器具',
                 },
                 {
                     classLst: ['col', 'mt-2'],
-                    imgSrc: 'https://fakeimg.pl/600x600/?text=600x600(4)',
+                    imgSrc: 'images\\原創商品.jpg',
                     name: '原創商品',
                 },
             ],
             infos: [
                 {
                     classLst: ['col'],
-                    imgSrc: 'https://fakeimg.pl/600x400/?text=600x400(1)',
+                    imgSrc: 'images\\夏季配方.jpg',
                     title: '夏季配方冰鎮咖啡開始販售',
                     date: '2022-05-15',
                     content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
                 },
                 {
                     classLst: ['col', 'd-none', 'd-md-block'],
-                    imgSrc: 'https://fakeimg.pl/600x400/?text=600x400(2)',
+                    imgSrc: 'images\\父親節.jpg',
                     title: '父親節特惠活動開跑',
                     date: '2022-06-12',
                     content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
                 },
                 {
                     classLst: ['col', 'd-none', 'd-md-block'],
-                    imgSrc: 'https://fakeimg.pl/600x400/?text=600x400(3)',
+                    imgSrc: 'images\\年中慶.jpg',
                     title: '年中慶即將結束',
                     date: '2022-07-09',
                     content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
